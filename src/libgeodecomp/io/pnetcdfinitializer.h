055a3438b39166aea2f7fd480b91718e5477ab51
Arno Proeme <a.proeme@epcc.ed.ac.uk>
2021-05-13 01:14:45 +0100

    Multiple netCDF sources to enable e.g. restart from water_depth netCDF

--- src/libgeodecomp/io/pnetcdfinitializer.h
+++ src/libgeodecomp/io/pnetcdfinitializer.h
@@ -23,6 +23,23 @@ using namespace PnetCDF::exceptions;
 
 namespace LibGeoDecomp {
 
+
+/**
+ * Helper struct that defines how an application can provide
+ * information specifying which netCDF source (filename, netCDF
+ * variable name) is to be used to initialise a particular named grid
+ * (i.e. Cell Member) variable. The PnetCDFInitializer constructor
+ * above expects a vector of an arbitray number of these sources.
+ */
+template<typename CELL_TYPE>
+struct netCDFSource
+{
+    std::string file;
+    std::string variableName;
+    Selector<CELL_TYPE> selector; // Selector describing target destination for data
+};
+    
+    
 /**
  *
  * Initialises a specific grid variable in parallel for all grid Cells by reading
@@ -41,40 +58,59 @@ public:
     static const int DIM = Topology::DIM;
     
     explicit PnetCDFInitializer(
-	const std::string& file,
-	const Selector<CELL_TYPE>& selector,
-	const std::string& ncVariableName,
+	const std::vector<netCDFSource<CELL_TYPE>>& netCDFSources,
 	const unsigned steps) : 
-	file(file),
-	selector(selector),
-	ncVariableName(ncVariableName),
+	netCDFSources(netCDFSources),
 	steps(steps)
 	{
-	    readHeader();
+	    readHeaders();
 	}
-
-    void readHeader()
+    
+    void readHeaders()
 	{
-	    NcmpiFile ncFile(MPI_COMM_WORLD,
-			     file,
-			     NcmpiFile::FileMode::read,
-			     NcmpiFile::FileFormat::classic2);
+	    std::vector<Coord<DIM>> sourceDimensions(netCDFSources.size());
 	    
-	    // To do: generalise (dimensions, dimension names, including long/lat)
-	    NcmpiDim yDim = ncFile.getDim("y");
-	    NcmpiDim xDim = ncFile.getDim("x");
+	    for(size_t i=0; i<netCDFSources.size(); i++)
+	    {
+		NcmpiFile ncFile(MPI_COMM_WORLD,
+				 netCDFSources[i].file,
+				 NcmpiFile::FileMode::read,
+				 NcmpiFile::FileFormat::classic2);
 	    
-	    MPI_Offset yExtent = yDim.getSize();
-	    MPI_Offset xExtent = xDim.getSize();
+		// To do: generalise (dimensions, dimension names, including long/lat)
+		NcmpiDim yDim = ncFile.getDim("y");
+		NcmpiDim xDim = ncFile.getDim("x");
+		
+		MPI_Offset yExtent = yDim.getSize();
+		MPI_Offset xExtent = xDim.getSize();
+		
+		sourceDimensions[i] = Coord<DIM>(xExtent, yExtent);
+		
+		if(i>0)
+		{
+		    if (sourceDimensions[i] != sourceDimensions[0])
+		    {
+			if (MPILayer().rank() == 0)
+			{
+			    std::cout << "Error: dimensions of netCDF files do not all match" << std::endl;
+			}
+			
+			return;
+		    }
+		}
+
+		// Add copy constructor to Coord class? 
+		globalDimensions = Coord<2>(sourceDimensions[0][0], sourceDimensions[0][1]));
 	    
-	   globalDimensions = Coord<DIM>(xExtent, yExtent);
-	    
-	    if (MPILayer().rank() == 0) {
-		std::cout << "Initialising grid from: " + file << std::endl;
-		std::cout << "Dimensions: " + globalDimensions.toString() << std::endl; 
+		if (MPILayer().rank() == 0)
+		{
+		    std::cout << "Dimensions: " + globalDimensions.toString() << std::endl; 
+		}
+		
 	    }
 	}
-
+    
+    
     
     Coord<DIM> gridDimensions() const
 	{
@@ -90,25 +126,38 @@ public:
 	{
 	    return 0;
 	}
+
     
     virtual void grid(GridBase<CELL_TYPE, DIM> *localGrid)
 	{
 	    Region<DIM> region;
 	    region << localGrid->boundingBox();
-	    initialiseRegion(region, localGrid, file);
+
+	    for(size_t i=0; i<netCDFSources.size(); i++)
+	    {
+		initialiseRegion(netCDFSources[i], region, localGrid);
+	    }
 	} 
+
     
     template<typename GRID_TYPE, int DIM>
-    void initialiseRegion(const Region<DIM>& region,
-			  GRID_TYPE* localGrid,
-			  const std::string& file)
+    void initialiseRegion(const netCDFSource<CELL_TYPE>& ncSource,
+			  const Region<DIM>& region,
+			  GRID_TYPE* localGrid)
 	{
+	    if (MPILayer().rank() == 0)
+	    {
+		std::cout << "Initialising grid variable " << ncSource.selector.name()
+			  <<  " from netCDF variable " + ncSource.variableName
+			  << " in " << ncSource.file << std::endl;
+	    }
+
 	    NcmpiFile ncFile(MPI_COMM_WORLD,
-			     file,
+			     ncSource.file,
 			     NcmpiFile::FileMode::read,
 			     NcmpiFile::FileFormat::classic2);
 	    
-	    NcmpiVar ncVar = ncFile.getVar(ncVariableName);
+	    NcmpiVar ncVar = ncFile.getVar(ncSource.select.variableName);
 
 	    // get the first cell in this rank's grid,
 	    // determine its x and y coordinates,
@@ -122,7 +171,7 @@ public:
 	    start[1] = box.origin.x();
 	    count[0] = box.dimensions.y();
 	    count[1] = box.dimensions.x();
-
+	    
 	    // Uncomment to debug
 	    //std::ostringstream debug1;
 	    //debug1 << "NetCDF read rank" << MPILayer().rank() << ": localGrid->boundingBox() = " << localGrid->boundingBox() <<  ", start=" << start << ", count=" << count << std::endl;
@@ -130,29 +179,27 @@ public:
 	    
 	    std::vector<double> buffer;
 	    buffer.resize(count[0]*count[1]);
-
+	    
 	    ncVar.getVar_all(start, count, &buffer[0]);
-
+	    
 	    // Uncomment to debug
 	    //std::ostringstream debug2;
 	    //debug2 << "NetCDF read rank" << MPILayer().rank() << " netCDF read buffer: " << buffer << std::endl;
 	    //std::cout << debug2.str();
 	    
-	    localGrid->loadMember(&buffer[0], MemoryLocation::HOST, selector, region);
+	    localGrid->loadMember(&buffer[0], MemoryLocation::HOST, ncSource.selector, region);
 	}
-		    
+    
     
 protected:
     Coord<DIM> globalDimensions;    
     unsigned steps;
-
+    
 private:
-    std::string file;
-    std::string ncVariableName;
-    Selector<CELL_TYPE> selector;
-
+    std::vector<netCDFSource<CELL_TYPE>> netCDFSources;
+    
 };
-
+    
 }
 
 #endif 
